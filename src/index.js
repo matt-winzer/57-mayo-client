40049bf1e46b07ccc2fc012c383e9ccdf506c24d
Matt Winzer <matt.winzer@gmail.com>
2018-11-27 10:06:06 -0700

    added semantic-ui-css and node-sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles.global.scss';
 import './index.css';
+
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
