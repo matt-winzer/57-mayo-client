2e7be6c977f8c786421affc2983b4ad9d2e86a48
Matt Winzer <matt.winzer@gmail.com>
2018-12-09 04:28:19 -0700

    added HeaderCard to Signup

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {
-  Header,
   Card
 } from 'semantic-ui-react'
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
-import { Button, Card, Form, Message } from 'semantic-ui-react'
+import { Redirect } from 'react-router-dom'
+import {
+  Button,
+  Card,
+  Form,
+  Message,
+  Grid
+} from 'semantic-ui-react'
+
+// Utilities
 import jwtDecode from 'jwt-decode'
 import { fetchPost } from '../lib/fetch'
 import * as auth from '../lib/authService'
 
-import { Redirect } from 'react-router-dom'
+// Components
+import HeaderCard from './HeaderCard'
 
 class Signup extends Component {
   state = {
@@ -50,33 +60,43 @@ class Signup extends Component {
     if (user.id) return <Redirect to='/reviews' />
 
     return (
-      <Card fluid>
-        <Card.Content>
-          <Card.Header>Signup</Card.Header>
-          <Form onSubmit={handleSubmit}>
-            <Form.Field>
-              <label>Email</label>
-              <input placeholder='Email'
-                type='email'
-                name='email'
-                onChange={handleChange}
-                value={email}
-              />
-            </Form.Field>
-            <Form.Field>
-              <label>Password</label>
-              <input placeholder='Password'
-                type='password'
-                name='password'
-                onChange={handleChange}
-                value={password}
-              />
-            </Form.Field>
-            <Button positive type='submit'>Signup</Button>
-          </Form>
-          {message && <Message error>{message}</Message>}
-        </Card.Content>
-      </Card>
+      <Grid stackable>
+        <Grid.Row centered>
+          <Grid.Column mobile={16} tablet={12} computer={8}>
+            <HeaderCard
+              header={'Metro Monitor Signup'}
+              subheader={'Create a New Account'}
+            />
+          </Grid.Column>
+        </Grid.Row>
+        <Card fluid>
+          <Card.Content>
+            <Card.Header>Signup</Card.Header>
+            <Form onSubmit={handleSubmit}>
+              <Form.Field>
+                <label>Email</label>
+                <input placeholder='Email'
+                  type='email'
+                  name='email'
+                  onChange={handleChange}
+                  value={email}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>Password</label>
+                <input placeholder='Password'
+                  type='password'
+                  name='password'
+                  onChange={handleChange}
+                  value={password}
+                />
+              </Form.Field>
+              <Button positive type='submit'>Signup</Button>
+            </Form>
+            {message && <Message error>{message}</Message>}
+          </Card.Content>
+        </Card>
+      </Grid>
     )
   }
 }
