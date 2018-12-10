7197aac22960f4dd58bfd9517f0b6290985e6e4b
Matt Winzer <matt.winzer@gmail.com>
2018-12-09 19:33:51 -0700

    added detailed cards to InfoPage

--- src/components/Information.js
+++ src/components/Information.js
@@ -3,13 +3,14 @@ import {
   Grid,
   Button,
   Card,
-  Embed
+  List,
+  Divider
 } from 'semantic-ui-react'
 
 // Components
 import HeaderCard from './HeaderCard'
 import InfoCard from './InfoCard'
-import ViewPDF from './ViewPDF'
+import PDFCard from './PDFCard'
 
 const Information = () => {
   return (
@@ -18,15 +19,125 @@ const Information = () => {
         <Grid.Column mobile={16} tablet={12} computer={8}>
           <HeaderCard
             header={'Metro Monitor Information'}
-            subheader={'Product Features'}
+            subheader={'Product Features & Sample'}
           />
         </Grid.Column>
       </Grid.Row>
       <Grid.Row centered>
           <Button size={'massive'} color={'violet'}>Subscribe Now</Button>
       </Grid.Row>
+      <Grid.Row columns={3}>
+
+        <Grid.Column>
+          <Card fluid>
+            <Card.Content>
+              <Card.Header textAlign={'center'}>Vital Insight</Card.Header>
+            </Card.Content>
+            <Card.Content className='info-card-content'>
+
+              <List>
+                <List.Item>Target Rent Range
+                  <List bulleted>
+                    <List.Item>Where the largest concentration of renters is found</List.Item>
+                  </List>
+                </List.Item>
+                <Divider />
+                <List.Item>Expected Returns From
+                  <List bulleted>
+                    <List.Item>Renting (Cash on Cash)</List.Item>
+                    <List.Item>Equity Appreciation</List.Item>
+                    <List.Item>High - Medium - Low</List.Item>
+                  </List>
+                </List.Item>
+                <Divider />
+                <List.Item>Risk
+                  <List bulleted>
+                    <List.Item>Future Falling Prices</List.Item>
+                    <List.Item>Insufficient Rent</List.Item>
+                    <List.Item>High - Medium - Low</List.Item>
+                  </List>
+                </List.Item>
+              </List>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+
+        <Grid.Column>
+          <Card fluid>
+            <Card.Content>
+              <Card.Header textAlign={'center'}>Expert Analysis</Card.Header>
+            </Card.Content>
+            <Card.Content className='info-card-content'>
+              <List>
+                <List.Item>Positive & Negative Features of the Current Situation
+                    <List bulleted>
+                    <List.Item>In plain language</List.Item>
+                  </List>
+                </List.Item>
+                <Divider />
+                <List.Item>3 Concise Paragraphs
+                    <List bulleted>
+                    <List.Item>Investment Strategies</List.Item>
+                    <List.Item>The Local Economy</List.Item>
+                    <List.Item>Investment Risk</List.Item>
+                  </List>
+                </List.Item>
+                <Divider />
+                <List.Item>3 Essential Trend Charts
+                    <List bulleted>
+                    <List.Item>Home Price Change</List.Item>
+                    <List.Item>Job Growth Rate</List.Item>
+                    <List.Item>Home Price vs Income Price</List.Item>
+                    {/* <List.Item>See if a market is OVER-PRICED</List.Item> */}
+                  </List>
+                </List.Item>
+              </List>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+
+        <Grid.Column>
+          <Card fluid>
+            <Card.Content>
+              <Card.Header textAlign={'center'}>Crucial Depth</Card.Header>
+            </Card.Content>
+            <Card.Content className='info-card-content'>
+              <List>
+                {/* <List.Item>Positive & Negative Features of the Current Situation
+                    <List bulleted>
+                    <List.Item>In plain language</List.Item>
+                  </List>
+                </List.Item>
+                <Divider /> */}
+                <List.Item>Current Average
+                    <List bulleted>
+                    <List.Item>Home Price</List.Item>
+                    <List.Item>Monthly Rent</List.Item>
+                    <List.Item>3-year Forecast for each</List.Item>
+                  </List>
+                </List.Item>
+                <Divider />
+                <List.Item>County Sub-Markets
+                    <List bulleted>
+                    <List.Item>Target Rent Range</List.Item>
+                    <List.Item>Home Prices</List.Item>
+                    <List.Item>Rents and the Ratio</List.Item>
+                    <List.Item>3-year Forecast for each</List.Item>
+                    <List.Item>Job Growth in the past 2 years</List.Item>
+                  </List>
+                </List.Item>
+              </List>
+            </Card.Content>
+            <Card.Content extra>
+              2 year Job Growth is an important way to compare sub-markets for the strength of demand for housing - for both homes and rentals.
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+        <Grid.Column tablet={8} computer={16}>
+          <PDFCard header='Sample Review' path={'/austin-tx.pdf'} />
+        </Grid.Column>
+      </Grid.Row>
       <Grid.Row>
-        <ViewPDF path={'/austin-tx.pdf'} />
       </Grid.Row>
     </Grid>
   )
